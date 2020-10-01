3a50f723c0445f7a7f7605e0470150494a439d7a
Abdul Samad <53661372+AbSamDev@users.noreply.github.com>
2020-10-01 15:14:52 +0500

    added prototypes

--- maxSubarray.cpp
+++ maxSubarray.cpp
@@ -1,1 +1,13 @@
-//blank
+#include <iostream>
+using namespace std;
+
+
+// ProtoTypes
+void find_maximum_subarray_brute(int array[], int low,int high, int total, int size);
+void RandomArray(int array[], int size);
+
+
+int main(){
+	//here the code begins☻
+	return 0;
+}
